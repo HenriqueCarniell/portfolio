a88d390c13b26b122af3a077116beb851f59a8d1
Gustavo Henrique Queiroz Carniel <henriquecarniel@dinfor.uniceub>
2023-09-15 10:15:39 -0300

    Adicionei um título na página

---
+++ src/components/portfolio/portfolio.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Header from '../header/header';
+
+import './portfolio.css';
+
+let Main = () => {
+    return (
+        <div>
+            <Header/>
+            <div id="banner1">
+
+            </div>
+            <div id="cards">
+                <div className='card'>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+                <div className='card'>
+                    
+                    </div>
+                    <div className='card'>
+                    
+                    </div>
+                    <div className='card'>
+                    
+                    </div>
+                    <div className='card'>
+                    
+                    </div>
+                    <div className='card'>
+                    
+                    </div>
+                    <div className='card'>
+                    
+                    </div>
+            </div>
+        </div>
+    );
+}
+
+export { Main };
